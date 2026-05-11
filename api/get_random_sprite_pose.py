8c395e122494933fded483e2b092d5e9151ca779
jazer <jazer2187@gamil.com>
2026-05-10 19:41:57 -0700

    seed rng for getting a random sprite

--- api/get_random_sprite_pose.py
+++ api/get_random_sprite_pose.py
@@ -4,6 +4,10 @@ def get_random_sprite_pose():
   from graphics.sprites.sprites import id_sprite
   from graphics.palettes.palettes import id_palette
 
+  from seed import generate_seed
+  seed = generate_seed()
+  random.seed(seed)
+
   CHARACTER_POSES = [1, 9, 10, 11, 16, 20, 22, 24, 29, 31, 32, 36]
   
   sprite = random.choice(list(id_sprite.keys()))
